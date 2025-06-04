4fbaf6be67c9fb28ffe76131d0ad59d24caf5059
Eduardo Juan <lucky36616@gmail.com>
2025-06-04 01:23:15 -0300

    Update app.js

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,3 +2,8 @@ const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('scroll', window.scrollY > 450);
+});
