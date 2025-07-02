b0be26ebebf1b5338326395c073de5e3e5109ca9
Eduardo Juan <lucky36616@gmail.com>
2025-07-02 17:54:50 -0300

    Add files via upload

---
+++ assets/js/carrossel.js
@@ -0,0 +1,58 @@
+const carousels = {
+    nokia: {
+        currentSlide: 0,
+        trackSelector: '.carousel-track1',
+        dotSelector: '.dot'
+    },
+    canon: {
+        currentSlide: 0,
+        trackSelector: '.carousel-track2',
+        dotSelector: '.dot'
+    },
+    whirlpool: {
+        currentSlide: 0,
+        trackSelector: '.carousel-track3',
+        dotSelector: '.dot'
+    },
+    lenovo: {
+        currentSlide: 0,
+        trackSelector: '.carousel-track4',
+        dotSelector: '.dot'
+    }
+};
+
+function moveCarousel(carouselId, direction) {
+    const carousel = document.getElementById(carouselId);
+    const carouselData = carousels[carouselId];
+    const track = carousel.querySelector(carouselData.trackSelector);
+    const slides = track.children;
+    const dots = carousel.querySelectorAll(carouselData.dotSelector);
+
+    carouselData.currentSlide += direction;
+
+    if (carouselData.currentSlide < 0) {
+        carouselData.currentSlide = slides.length - 1;
+    } else if (carouselData.currentSlide >= slides.length) {
+        carouselData.currentSlide = 0;
+    }
+
+    track.style.transform = `translateX(-${carouselData.currentSlide * 100}%)`;
+
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[carouselData.currentSlide].classList.add('active');
+}
+
+function goToSlide(carouselId, slideIndex) {
+    const carousel = document.getElementById(carouselId);
+    const carouselData = carousels[carouselId];
+    const track = carousel.querySelector(carouselData.trackSelector);
+    const dots = carousel.querySelectorAll(carouselData.dotSelector);
+
+    carouselData.currentSlide = slideIndex;
+
+    track.style.transform = `translateX(-${carouselData.currentSlide * 100}%)`;
+
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[carouselData.currentSlide].classList.add('active');
+}
+
